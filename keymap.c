11304916eb4e17db9eeb4e55eb0051804deefc95
Georgy Moshkin <louielouie314@gmail.com>
2020-04-19 20:50:46 +0300

    fuck these bitches

--- keymap.c
+++ keymap.c
@@ -36,11 +36,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BAS] = LAYOUT_ergodox(
     // Left hand
-    KC_EQL      , KC_1     , KC_2    , KC_3   , KC_4    , KC_5 , KC_ESC  ,
-    KC_TAB      , KC_Q     , KC_W    , KC_E   , KC_R    , KC_T , TG(SYM) ,
-    CT(KC_BSPC) , KC_A     , KC_S    , KC_D   , KC_F    , KC_G ,
-    KC_LSFT     , CT(KC_Z) , KC_X    , KC_C   , KC_V    , KC_B , KC_LBRC ,
-    MO(SYM)     , KC_GRV   , KC_QUOT , KC_LEFT, KC_RGHT ,
+    KC_EQL      , KC_1    , KC_2    , KC_3   , KC_4    , KC_5 , KC_ESC  ,
+    KC_TAB      , KC_Q    , KC_W    , KC_E   , KC_R    , KC_T , TG(SYM) ,
+    CT(KC_BSPC) , KC_A    , KC_S    , KC_D   , KC_F    , KC_G ,
+    KC_LSFT     , KC_Z    , KC_X    , KC_C   , KC_V    , KC_B , KC_LBRC ,
+    MO(SYM)     , KC_GRV  , KC_QUOT , KC_LEFT, KC_RGHT ,
 
                AT(KC_APP) , KC_LGUI ,
                             KC_END  ,
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_DEL  , KC_6 , KC_7  , KC_8    , KC_9    , KC_0           , KC_MINS         ,
     KC_BSPC , KC_Y , KC_U  , KC_I    , KC_O    , KC_P           , KC_BSLS         ,
               KC_H , KC_J  , KC_K    , KC_L    , T_MED(KC_SCLN) , RGUI_T(KC_QUOT) ,
-    KC_RBRC , KC_N , KC_M  , KC_COMM , KC_DOT  , CT(KC_SLSH)    , KC_RSFT         ,
+    KC_RBRC , KC_N , KC_M  , KC_COMM , KC_DOT  , KC_SLSH        , KC_RSFT         ,
                      KC_UP , KC_DOWN , KC_LBRC , KC_RBRC        , MO(SYM)         ,
 
     KC_DEL  , CT(KC_ESC) ,
