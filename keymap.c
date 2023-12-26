d4fd3c3d1b65d6b87feb9d8cf672f3186c023342
Georgy Moshkin <gmoshkin@picodata.io>
2023-12-26 12:49:13 +0300

    alt on the right side

--- keymap.c
+++ keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_BSPC , KC_Y , KC_U  , KC_I    , KC_O    , KC_P         , KC_BSLS     ,
               KC_H , KC_J  , KC_K    , KC_L    , T_M(KC_SCLN) , WT(KC_QUOT) ,
     KC_RBRC , KC_N , KC_M  , KC_COMM , KC_DOT  , KC_SLSH      , KC_RSFT     ,
-                     KC_UP , KC_DOWN , KC_LBRC , KC_RBRC      , MO(SYM)     ,
+                     KC_UP , KC_DOWN , KC_LBRC , KC_RBRC      , KC_LALT      ,
 
     KC_PGUP , KC_LCTL ,
     KC_PGDN ,
