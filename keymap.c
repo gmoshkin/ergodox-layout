f28a731d6d50b157444b37a34b5eb3c78a6ec678
Georgy Moshkin <gmoshkin@picodata.io>
2025-03-16 00:32:53 +0300

    replace unused '[' & ']' with sym (so that F1-F12 keys are easier) + remove weird includes

--- keymap.c
+++ keymap.c
@@ -1,16 +1,5 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
-#include "keymap_german.h"
-#include "keymap_nordic.h"
-#include "keymap_french.h"
-#include "keymap_spanish.h"
-#include "keymap_hungarian.h"
-#include "keymap_swedish.h"
-#include "keymap_br_abnt2.h"
-#include "keymap_canadian_multilingual.h"
-#include "keymap_german_ch.h"
-#include "keymap_jp.h"
-#include "keymap_bepo.h"
 
 enum custom_keycodes {
   RGB_SLD = EZ_SAFE_RANGE,
@@ -45,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EQL      , KC_1    , KC_2    , KC_3   , KC_4    , KC_5 , KC_ESC  ,
     KC_TAB      , KC_Q    , KC_W    , KC_E   , KC_R    , KC_T , KC_DEL  ,
     CT(KC_BSPC) , KC_A    , KC_S    , KC_D   , KC_F    , KC_G ,
-    KC_LSFT     , KC_Z    , KC_X    , KC_C   , KC_V    , KC_B , KC_LBRC ,
+    KC_LSFT     , KC_Z    , KC_X    , KC_C   , KC_V    , KC_B , MO(SYM) ,
     MO(SYM)     , KC_GRV  , KC_QUOT , KC_LEFT, KC_RGHT ,
 
                KC_APP     , KC_END  ,
@@ -56,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC  , KC_6 , KC_7  , KC_8    , KC_9    , KC_0         , KC_MINS     ,
     KC_BSPC , KC_Y , KC_U  , KC_I    , KC_O    , KC_P         , KC_BSLS     ,
               KC_H , KC_J  , KC_K    , KC_L    , T_M(KC_SCLN) , WT(KC_QUOT) ,
-    KC_RBRC , KC_N , KC_M  , KC_COMM , KC_DOT  , KC_SLSH      , KC_RSFT     ,
-                     KC_UP , KC_DOWN , KC_LBRC , KC_RBRC      , KC_LALT      ,
+    MO(SYM) , KC_N , KC_M  , KC_COMM , KC_DOT  , KC_SLSH      , KC_RSFT     ,
+                     KC_UP , KC_DOWN , KC_LBRC , KC_RBRC      , KC_LALT     ,
 
     KC_PGUP , KC_LCTL ,
     KC_PGDN ,
