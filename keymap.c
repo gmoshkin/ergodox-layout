863796662a78d40eb9104f080fd93047e2ed7848
Georgy Moshkin <gmoshkin@picodata.io>
2025-03-24 13:10:16 +0300

    numpad digits on the SYM layer

--- keymap.c
+++ keymap.c
@@ -73,11 +73,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RGB_VAD , RGB_VAI , _______ ,
 
     // Right hand
-    _______ , KC_F6   , KC_F7   , KC_F8  , KC_F9 , KC_F10  , KC_F11  ,
-    _______ , KC_UP   , KC_7    , KC_8   , KC_9  , KC_ASTR , KC_F12  ,
-              KC_DOWN , KC_4    , KC_5   , KC_6  , KC_PLUS , KC_LGUI ,
-    _______ , KC_AMPR , KC_1    , KC_2   , KC_3  , KC_BSLS , _______ ,
-                        TG(SWP) , KC_DOT , KC_0  , KC_EQL  , _______ ,
+    _______ , KC_F6   , KC_F7   , KC_F8  , KC_F9  , KC_F10  , KC_F11  ,
+    _______ , KC_UP   , KC_KP_7 , KC_KP_8, KC_KP_9, KC_ASTR , KC_F12  ,
+              KC_DOWN , KC_KP_4 , KC_KP_5, KC_KP_6, KC_PLUS , KC_LGUI ,
+    _______ , KC_AMPR , KC_KP_1 , KC_KP_2, KC_KP_3, KC_BSLS , _______ ,
+                        TG(SWP) , KC_DOT , KC_KP_0, KC_EQL  , _______ ,
 
     RGB_TOG            , RGB_SLD ,
     TOGGLE_LAYER_COLOR ,
@@ -137,7 +137,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_MINS     , KC_6      , KC_7    , KC_8    , KC_9    , KC_0    , KC_ESC  ,
     KC_BSLS     , KC_Y      , KC_U    , KC_I    , KC_O    , KC_P    , KC_BSPC ,
     WT(KC_QUOT) , T_M(KC_H) , KC_J    , KC_K    , KC_L    , KC_SCLN ,
-    KC_RSFT     , KC_N      , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RBRC ,
+    KC_RSFT     , KC_N      , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , MO(SYM) ,
     TG(SWP)     , KC_LBRC   , KC_RBRC , KC_DOWN , KC_UP   ,
 
               KC_LCTL , KC_PGUP ,
@@ -148,7 +148,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC  , KC_1 , KC_2    , KC_3    , KC_4   , KC_5    , KC_EQL      ,
     KC_DEL  , KC_Q , KC_W    , KC_E    , KC_R   , KC_T    , KC_TAB      ,
               KC_A , KC_S    , KC_D    , KC_F   , KC_G    , CT(KC_BSPC) ,
-    KC_LBRC , KC_Z , KC_X    , KC_C    , KC_V   , KC_B    , KC_LSFT     ,
+    MO(SYM) , KC_Z , KC_X    , KC_C    , KC_V   , KC_B    , KC_LSFT     ,
                      KC_LEFT , KC_RGHT , KC_GRV , KC_QUOT , TG(SWP)     ,
 
     KC_END  , KC_APP     ,
